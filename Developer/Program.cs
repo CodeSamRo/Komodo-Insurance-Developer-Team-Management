13795f060e7f43fba120dc42a3a18ba7a3d5df85
Samuel <codesamro@gmail.com>
2021-10-16 16:48:10 -0400

    Add project files.

---
+++ Developer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Developer Name
+            //Developer ID
+            //method to identify one developer without mistake and also whether or not they have access to tool: Pluralsight
+            //Teams need their team MEMBERS and team NAME along with team ID
+            //Add member to team by unique identifier
+            //Delete member from team by unique identifier
+            //See list of existing developers to choose from and add to existing teams
+            //Manager will create a team, and then add developers individually from the developer dictory to that team
+            
+        }
+    }
+}
